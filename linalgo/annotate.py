a96938af6a80de0f25bcbeb98ea69b2ca217b8c4
Arnaud Rachez <arnaud.rachez@gmail.com>
2018-06-09 22:08:19 +0200

    ENH First basic version of models and client calls.

--- linalgo/annotate.py
+++ linalgo/annotate.py
@@ -1,4 +1,128 @@
+from collections import defaultdict, MutableSequence
+from mock.mock import self
+
+
+class Annotation:
+
+    def __init__(self, uri, type_id, task_id, text, owner, document_id,
+                 annotation_id=None):
+        self.id = annotation_id
+        self.uri = uri
+        self.type_id = type_id
+        self.task_id = task_id
+        self.text = text
+        self.owner = owner
+        self.document_id = document_id
+
+    def __repr__(self):
+        return str(self.type_id)
+
+
+class Annotations(MutableSequence):
+
+    def __init__(self, annotations=None):
+        super(Annotations, self).__init__()
+        self._list = []
+        self._user_index = defaultdict(list)
+        self._type_index = defaultdict(list)
+        for annotation in annotations:
+            self.append(annotation)
+
+    def __repr__(self):
+        return "<{0} {1}>".format(self.__class__.__name__, self._list)
+
+    def __len__(self):
+        return len(self._list)
+
+    def __getitem__(self, ii):
+        return self._list[ii]
+
+    def __delitem__(self, ii):
+        del self._list[ii]
+
+    def __setitem__(self, ii, val):
+        self._list[ii] = val
+
+    def __str__(self):
+        return str(self._list)
+
+    def insert(self, ii, val):
+        self._list.insert(ii, val)
+
+    def append(self, val):
+        self.insert(len(self._list), val)
+        self._user_index[val.owner].append(val)
+        self._type_index[val.type_id].append(val)
+
+
+class Annotator:
+
+    def __init__(self, name):
+        self.name = name
+
+    def annotate(self, document, annotation):
+        document.annotations.add(annotation)
+
+
 class Document:
 
-    def __init__(self, body, annotations=None):
-        pass
+    def __init__(self, name, content, corpus, metadata=None, document_id=None):
+        self.name = name
+        self.content = content
+        self.corpus = corpus
+        self.metadata = metadata
+        self.id = document_id
+        self._annotations = Annotations([])
+
+    @property
+    def labels(self):
+        return set(annotation.type_id for annotation in self.annotations)
+
+    @property
+    def annotations(self):
+        return self._annotations
+
+    @annotations.setter
+    def annotations(self, values):
+        self._annotations = Annotations(values)
+
+
+class Task:
+
+    def __init__(self, name, description, entities=None, corpora=None,
+                 annotators=None, annotations=[], documents=[], task_id=None):
+        self.id = task_id
+        self.name = name
+        self.description = description
+        self.entities = entities
+        self.corpora = corpora
+        self.annotators = annotators
+        self._annotations = annotations
+        self._documents = documents
+
+    @property
+    def documents(self):
+        return list(self._documents.values())
+
+    @documents.setter
+    def documents(self, values):
+        self._documents = dict((doc.id, doc) for doc in values)
+
+    @property
+    def annotations(self):
+        return self._annotations
+
+    @annotations.setter
+    def annotations(self, values):
+        self._annotations = Annotations(values)
+        for annotation in self.annotations:
+            doc_id = annotation.document_id
+            self._documents[doc_id].annotations.append(annotation)
+
+    def __repr__(self):
+        rep = (f"name: {self.name}\ndescription: {self.description}\n# "
+               f"documents: {len(self.documents)}\n# annotations: "
+               f"{len(self.annotations)}")
+        if self.id:
+            rep = f"id: {self.id}\n" + rep
+        return rep
--- linalgo/client.py
+++ linalgo/client.py
@@ -2,12 +2,50 @@ import json
 
 from http.client import HTTPConnection, HTTPSConnection
 
+from .annotate import Annotation, Document, Task
+
+
+def json2anno(js):
+    return Annotation(
+            uri=js['uri'],
+            document_id=js['document'],
+            type_id=js['type'],
+            task_id=js['group'],
+            text=js['text'],
+            owner=js['owner']
+        )
+
+
+def json2doc(js):
+    return Document(
+        name=js['uri'],
+        content=js['content'],
+        corpus=js['corpus'],
+        metadata=js['metadata'],
+        document_id=js['id']
+    )
+
+
+def json2task(js):
+    return Task(
+        name=js['name'],
+        description=js['description'],
+        entities=js['entities'],
+        corpora=js['corpora'],
+        annotators=js['annotators'],
+        task_id=js['id']
+    )
+
 
 class LinalgoClient:
 
-    def __init__(self, client_id, client_secret):
+    def __init__(self, client_id, client_secret, audience="annotate-api",
+                 api_url="localhost:8000"):
+        self.api_url = api_url
+        self.audience = audience
         self.id = client_id
         self.secret = client_secret
+        self.authenticate()
 
     def authenticate(self):
         conn = HTTPSConnection("linalgo.eu.auth0.com")
@@ -15,7 +53,7 @@ class LinalgoClient:
         payload = json.dumps({
             'client_id': self.id,
             'client_secret': self.secret,
-            'audience': "annotate-api",
+            'audience': self.audience,
             'grant_type': "client_credentials"
         })
         conn.request("POST", "/oauth/token", payload, headers)
@@ -26,10 +64,34 @@ class LinalgoClient:
         self.expires_in = data['expires_in']
         self.token_type = data['token_type']
 
-    def get_tasks(self):
-        conn = HTTPConnection("localhost:8000")
+    def request(self, url):
+        conn = HTTPConnection(self.api_url)
         headers = {'authorization': f"Bearer {self.access_token}"}
-        conn.request("GET", "/tasks/4/documents/?page_size=100000", headers=headers)
+        conn.request("GET", url, headers=headers)
         res = conn.getresponse()
         data = res.read()
-        print(data.decode("utf-8"))
+        return data
+
+    def get_tasks(self):
+        url = "/tasks/"
+        tasks = []
+        res = json.loads(self.request(url))
+        for js in res['results']:
+            task_id = js['id']
+            task = self.get_task(task_id)
+            tasks.append(task)
+        return tasks
+
+    def get_task(self, task_id):
+        task_url = f"/tasks/{task_id}/"
+        task_json = json.loads(self.request(task_url))
+        task = json2task(task_json)
+        docs_url = f"/tasks/{task_id}/documents/?page_size=1000"
+        docs_json = json.loads(self.request(docs_url))
+        docs = [json2doc(doc_json) for doc_json in docs_json['results']]
+        task.documents = docs
+        annotations_url = f"/tasks/{task_id}/annotations/?page_size=100000"
+        ann_json = json.loads(self.request(annotations_url))
+        annotations = [json2anno(a_json) for a_json in ann_json['results']]
+        task.annotations = annotations
+        return task
