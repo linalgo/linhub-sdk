c8521f5ee1245b65efb96cf179f57c9e82b2e15d
Arnaud Rachez <arnaud.rachez@gmail.com>
2018-07-09 18:34:36 +0200

    issue #4 - bulk upload annotations * Corrected credential bug in issue #3 - invalid creadentials * Added upload functionality to the HttpClient

--- linalgo/client.py
+++ linalgo/client.py
@@ -1,6 +1,5 @@
 import json
-
-from http.client import HTTPConnection, HTTPSConnection
+import requests
 
 from .annotate import Annotation, Corpus, Document, Task
 
@@ -39,33 +38,26 @@ def json2task(js):
 
 class LinalgoClient:
 
-    def __init__(self, token, api_url="localhost:8000"):
+    def __init__(self, token, api_url="http://localhost:8000"):
         self.api_url = api_url
         self.access_token = token
-        self.authenticate()
 
-    def authenticate(self):
-        conn = HTTPConnection(self.api_url)
-        headers = {'content-type': "application/json"}
-        payload = json.dumps({
-            'token': self.access_token
-        })
-        conn.request("GET", "/auth/client-token/", payload, headers)
-        res = conn.getresponse()
-        return res.status == 200
-
-    def request(self, url):
-        conn = HTTPConnection(self.api_url)
-        headers = {'Authorization': "Token %s" % self.access_token}
-        conn.request("GET", url, headers=headers)
-        res = conn.getresponse()
-        data = res.read()
-        return data
+    def request(self, url, data=None):
+        url = self.api_url + url
+        headers = {'Authorization': f"Token {self.access_token}"}
+        res = requests.get(url, headers=headers)
+        if res.status_code == 401:
+            raise Exception(f"Authentication failed. Please check your token.")
+        if res.status_code == 404:
+            raise Exception(f"{url} not found.")
+        elif res.status_code != 200:
+            raise Exception(f"Request returned status {res.status_code}")
+        return res.json()
 
     def get_corpora(self):
         url = f"/corpora/"
         corpora = []
-        res = json.loads(self.request(url))
+        res = self.request(url)
         for js in res['results']:
             corpus_id = js['id']
             corpus = self.get_corpus(corpus_id)
@@ -74,7 +66,7 @@ class LinalgoClient:
 
     def get_corpus(self, corpus_id):
         url = f"/corpora/{corpus_id}/"
-        res = json.loads(self.request(url))
+        res = self.request(url)
         corpus = Corpus(name=res['name'], description=res['description'])
         documents = self.get_corpus_documents(corpus_id)
         corpus.documents = documents
@@ -82,7 +74,7 @@ class LinalgoClient:
 
     def get_corpus_documents(self, corpus_id):
         url = f"/corpora/{corpus_id}/documents/?page_size=100000"
-        res = json.loads(self.request(url))
+        res = self.request(url)
         documents = []
         for js in res['results']:
             document = json2doc(js)
@@ -92,7 +84,7 @@ class LinalgoClient:
     def get_tasks(self):
         url = "/tasks/"
         tasks = []
-        res = json.loads(self.request(url))
+        res = self.request(url)
         for js in res['results']:
             task_id = js['id']
             task = self.get_task(task_id)
@@ -101,18 +93,25 @@ class LinalgoClient:
 
     def get_task_documents(self, task_id):
         docs_url = f"/tasks/{task_id}/documents/?page_size=100000"
-        docs_json = json.loads(self.request(docs_url))
+        docs_json = self.request(docs_url)
         return [json2doc(doc_json) for doc_json in docs_json['results']]
 
     def get_task_annotations(self, task_id):
         annotations_url = f"/tasks/{task_id}/annotations/?page_size=100000"
-        ann_json = json.loads(self.request(annotations_url))
+        ann_json = self.request(annotations_url)
         return [json2anno(a_json) for a_json in ann_json['results']]
 
     def get_task(self, task_id):
         task_url = f"/tasks/{task_id}/"
-        task_json = json.loads(self.request(task_url))
+        task_json = self.request(task_url)
         task = json2task(task_json)
         task.documents = self.get_task_documents(task_id)
         task.annotations = self.get_task_annotations(task_id)
         return task
+
+    def upload(self, annotations):
+        url = '/annotations/'
+        url = self.api_url + url
+        headers = {'Authorization': f"Token {self.access_token}"}
+        res = requests.post(url, json=annotations, headers=headers)
+        print(res)
